217ac33de28cddde332ed3628ef4391b1fd3ddbc
Jared Mellentine <jared@mlntn.com>
2014-06-13 08:27:53 -0700

    Optional, because it makes no sense for a static memory cache

--- Cache/Provider.php
+++ Cache/Provider.php
@@ -6,7 +6,7 @@ abstract class Provider implements ProviderInterface {
 
   protected $ttl;
 
-  public function __construct($ttl) {
+  public function __construct($ttl = null) {
     $this->ttl = $ttl;
   }
 
